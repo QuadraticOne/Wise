448a74dae6db46de348ccc9a978001025ff33757
aatack <alexanderjtatack@gmail.com>
2018-09-18 02:19:50 +0100

    Add some frequently used loss metrics and optimisers

---
+++ wise/util/training.py
@@ -0,0 +1,101 @@
+from wise.util.tensors import placeholder_node
+import tensorflow as tf
+
+
+def regression_metrics(output_node_shape, output_node, variables=None):
+    """
+    [Int] -> tf.Tensor -> [tf.Variable]? -> (TargetNode, LossNode, Optimiser)
+    Create metrics - target node, loss node, and optimiser - for a
+    regression model.
+    """
+    node_name = output_node.name
+    target_node = placeholder_node(node_name + '.target', output_node_shape,
+        dynamic_dimensions=1)
+    loss_node = tf.losses.mean_squared_error(target_node, output_node)
+    optimiser = default_adam_optimiser(loss_node, variables=variables)
+    return target_node, loss_node, optimiser
+
+
+def classification_metrics(output_node_shape, output_node, variables=None):
+    """
+    [Int] -> tf.Tensor -> [tf.Variable]? ->
+        (TargetNode, LossNode, Accuracy, Optimiser)
+    Create metrics - target node, loss node, accuracy node, and optimiser -
+    for a classification model.
+    """
+    node_name = output_node.name
+    target_node = placeholder_node(node_name + '.target', output_node_shape,
+        dynamic_dimensions=1)
+    loss_node = tf.losses.log_loss(target_node, output_node)
+    accuracy_node = accuracy(output_node, target_node, name + '.accuracy')
+    optimiser = default_adam_optimiser(loss_node, variables=variables)
+    return target_node, loss_node, accuracy_node, optimiser
+
+
+def beta_variational_metrics(input_node, reconstruction_node, means_node,
+        stddevs_node, beta=1.0, variables=None, eps=1e-7):
+    """
+    tf.Tensor -> tf.Tensor -> tf.Tensor -> tf.Tensor -> Float? ->
+        [tf.Variable]? -> Float? -> (LossNode, LossNode, LossNode, Optimiser)
+    Create three different loss nodes (reconstruction, KL-divergence,
+    and composite) and an adam optimiser for a variational inference
+    model. 
+    """
+    reconstruction_loss, _ = reconstruction_metrics(input_node,
+        reconstruction_node, variables=[])
+    kl_loss, _ = variational_metrics(means_node, stddevs_node, variables=[])
+    composite_loss = reconstruction_loss + beta * kl_loss
+    optimiser = default_adam_optimiser(composite_loss, variables=variables)
+    return reconstruction_loss, kl_loss, composite_loss, optimiser
+
+
+def reconstruction_metrics(input_node, reconstruction_node, variables=None):
+    """
+    tf.Tensor -> tf.Tensor -> [tf.Variable]? -> (LossNode, Optimiser)
+    Create reconstruction metrics for an autoencoder, which penalises the
+    distance between an input and a reconstructed output in the vector
+    space.
+    """
+    loss_node = tf.losses.mean_squared_error(input_node, reconstruction_node)
+    optimiser = default_adam_optimiser(loss_node, variables=variables)
+    return loss_node, optimiser
+
+
+def variational_metrics(means_node, stddevs_node, variables=None, eps=1e-7):
+    """
+    tf.Tensor -> tf.Tensor -> [tf.Variable]? -> Float? -> (LossNode, Optimiser)
+    Create a KL-divergence loss for the Gaussian distributions described
+    by the mean and standard deviation nodes provided.  Measures the KL-divergence
+    from a standard Gaussian.
+    """
+    loss_node = tf.reduce_mean(0.5 * tf.reduce_mean(
+        tf.square(means_node) + tf.square(stddevs_node) - 
+            tf.log(tf.square(stddevs_node) + eps) - 1,1
+    ))
+    optimiser = default_adam_optimiser(loss_node, variables=variables)
+    return loss_node, optimiser
+
+
+def default_adam_optimiser(loss_node, variables=None):
+    """
+    LossNode -> Optimiser
+    Create an adam optimiser with default learning parameters set to minimise
+    the given loss node.
+    """
+    node_name = loss_node.name
+    return tf.train.AdamOptimizer(name=node_name + '.adam_optimiser')   
+        .minimize(loss_node, var_list=variables, name=node_name + '.adam_optimiser.minimise')
+
+
+def accuracy(prediction_node, target_node, name):
+    """
+    tf.Tensor -> tf.Tensor -> String -> tf.Tensor
+    Create a node which measures the accuracy of a predictor as compared
+    to a target node, where the prediction is considered correct if it is
+    on the same side of 0.5 as the target.
+    """
+    prediction_positive = tf.less(0.5, prediction_node)
+    target_positive = tf.less(0.5, target_node)
+    correct = tf.equal(prediction_positive, target_positive)
+    correct_floats = tf.cast(correct, tf.float32)
+    return tf.reduce_mean(correct_floats, name=name)
