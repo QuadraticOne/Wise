c60c309b12859c0ae9a7b0ca8c0b10380c981381
aatack <alexanderjtatack@gmail.com>
2018-10-18 17:09:02 +0100

    Implement layers with Gaussian distributed weights

---
+++ wise/networks/stochastic/gaussianweights.py
@@ -0,0 +1,67 @@
+from wise.networks.layer import Layer
+from wise.networks.activation import Activation
+from wise.networks.deterministic.feedforwardlayer import FeedforwardLayer
+from wise.util.tensors import glorot_initialised_vars
+import tensorflow as tf
+
+
+class GaussianWeightsLayer(Layer):
+    """
+    A neural network layer which, instead of having deterministic weights,
+    samples its weights from independently defined Gaussian distributions.
+    """
+
+    def __init__(self, name, session, input_shape, output_shape,
+            activation=Activation.DEFAULT, input_node=None, save_location=None):
+        """
+        String -> tf.Session -> [Int] -> [Int] -> (tf.Tensor -> String -> tf.Tensor)
+            -> tf.Tensor? -> String? -> Network
+        """
+        super().__init__(name, session, input_shape, output_shape,
+            input_node, save_location)
+
+        self.activation = activation
+
+        self.weight_means = None
+        self.weight_stddevs = None
+        self.weight_noise = None
+        self.weights = None
+        self.after_weights = None
+        self.biases = None
+        self.after_biases = None
+
+        self._initialise()
+
+    def _initialise(self):
+        """
+        () -> ()
+        Initialise the tensorflow tensors that make up the layer.
+        """
+        weight_shape = reversed(self.input_shape) + self.output_shape
+        self.weight_means = glorot_initialised_vars(
+            self.extend_name('weight_means'), weight_shape)
+        self.weight_stddevs = glorot_initialised_vars(
+            self.extend_name('weight_stddevs'), weight_shape)
+        self.weight_noise = tf.random_normal(weight_shape, 0, 1,
+            name=self.extend_name('noise'))
+        self.weights = tf.add(self.weight_means, tf.multiply(self.weight_noise,
+            self.weight_stddevs), name=self.extend_name('weights'))
+
+        self.after_weights = tf.tensordot(self.input_node, self.weights,
+            len(self.input_shape), name=self.extend_name('after_weights'))
+        self.biases = glorot_initialised_vars(self.extend_name('biases'), self.output_shape)
+        self.after_biases = tf.add(self.after_weights, self.biases,
+            name=self.extend_name('after_biases'))
+        self.output_node = self.activation(self.after_biases,
+            self.extend_name('output_node'))
+
+    def get_variables(self):
+        """
+        () -> [tf.Variable]
+        """
+        return [self.weight_means, self.weight_stddevs, self.biases]
+
+
+def reversed(ls):
+    return ls[::-1]
+
