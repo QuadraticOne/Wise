bf059f1c56270e43dcac321e3461586225ca98e4
aatack <alexanderjtatack@gmail.com>
2018-10-18 15:53:19 +0100

    Make variational networks inherit MLP

--- wise/networks/stochastic/variationalnetwork.py
+++ wise/networks/stochastic/variationalnetwork.py
@@ -1,10 +1,9 @@
-from wise.networks.network import Network
+from wise.networks.mlp import MLP
 from wise.networks.stochastic.variationallayer import VariationalLayer
-from wise.networks.deterministic.feedforwardnetwork import FeedforwardNetwork
+from wise.networks.deterministic.feedforwardlayer import FeedforwardLayer
 from wise.networks.activation import Activation
-from wise.util.tensors import placeholder_node
 
-class VariationalNetwork(Network):
+class VariationalNetwork(MLP):
     """
     Similar to a standard feedforward network but with a variational output
     layer.
@@ -19,56 +18,51 @@ class VariationalNetwork(Network):
             -> (tf.Tensor -> String -> tf.Tensor)? -> (tf.Tensor -> String -> tf.Tensor)?
             -> tf.Tensor? -> String? -> VariationalNetwork
         """
-        super().__init__(name, session, save_location)
-
-        self.input_shape = input_shape
-        self.layer_shapes = layer_shapes
         self.internal_activations = internal_activations
         self.means_activation = means_activation
         self.stddevs_activation = stddevs_activation
 
-        self.input_node = input_node
-        self.internal_network = None
-        self.output_layer = None
-        self.means_output_node = None
-        self.stddevs_output_node = None
-        self.sample_node = None
+        super().__init__(name, session, input_shape, self.make_constructors_generator,
+            layer_shapes, input_node, save_location)
 
-        self._initialise()
+        self.means_output_node = self.layers[-1].means_output_node
+        self.stddevs_output_node = self.layers[-1].stddevs_output_node
 
-    def _initialise(self):
+    def make_constructors_generator(self, n_layers):
         """
-        () -> ()
+        Int -> [String -> tf.Session -> [Int] -> [Int]
+            -> tf.Tensor? -> String? -> Layer]
+        Given the parameters passed to the network, when given the number
+        of layers the network will have, create a list of functions that
+        produce each layer given the layer size.
         """
-        if self.input_node is None:
-            self.input_node = placeholder_node(self.extend_name('input_node'),
-                self.input_shape, dynamic_dimensions=1)
-        
-        self.internal_network = FeedforwardNetwork(
-            name=self.extend_name('internal_network'),
-            session=self.get_session(),
-            input_shape=self.input_shape,
-            layer_shapes=self.layer_shapes[:-1],
-            activations=self.internal_activations,
-            input_node=self.input_node
-        )
+        def make_deterministic_constructor(activation):
+            def deterministic_constructor(name, session, input_shape,
+                    output_shape, input_node=None, save_location=None):
+                return FeedforwardLayer(
+                    name=name,
+                    session=session,
+                    input_shape=input_shape,
+                    output_shape=output_shape,
+                    activation=activation,
+                    input_node=input_node,
+                    save_location=save_location
+                )
+            return deterministic_constructor
 
-        self.output_layer = VariationalLayer(
-            name=self.extend_name('output_layer'),
-            session=self.get_session(),
-            input_shape=([self.input_shape] + self.layer_shapes)[-2],
-            output_shape=self.layer_shapes[-1],
-            means_activation=self.means_activation,
-            stddevs_activation=self.stddevs_activation,
-            input_node=self.internal_network.output_node
-        )
+        def variational_constructor(name, session, input_shape,
+                output_shape, input_node=None, save_location=None):
+            return VariationalLayer(
+                name=name,
+                session=session,
+                input_shape=input_shape,
+                output_shape=output_shape,
+                means_activation=self.means_activation,
+                stddevs_activation=self.stddevs_activation,
+                input_node=input_node,
+                save_location=save_location
+            )
 
-        self.means_output_node = self.output_layer.means_output_node
-        self.stddevs_output_node = self.output_layer.stddevs_output_node
-        self.sample_node = self.output_layer.sample_node
-
-    def get_variables(self):
-        """
-        () -> [tf.Variable]
-        """
-        return self.internal_network.get_variables() + self.output_layer.get_variables()
+        return [make_deterministic_constructor(activation) \
+            for activation in self.internal_activations(n_layers - 1)] + \
+            [variational_constructor]
