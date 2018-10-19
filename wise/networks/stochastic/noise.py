282fccfdd50549caa31d14063608b7286d9f553e
aatack <alexanderjtatack@gmail.com>
2018-10-19 02:22:04 +0100

    Implement and verify stochastic binary unit noise layer

--- wise/networks/stochastic/noise.py
+++ wise/networks/stochastic/noise.py
@@ -86,3 +86,30 @@ class ElementwiseGaussianNoiseLayer(NoiseLayer):
         noise = tf.random_normal(self.input_shape, name=self.extend_name('noise'))
         return tf.add(input_node, tf.multiply(stddevs, noise),
             name=self.extend_name('output_node')), [stddevs]
+
+
+class StochasticBinaryUnitLayer(NoiseLayer):
+    """
+    A layer which randomly turns off (sets to zero) each input
+    with a given probability.
+    """
+
+    def __init__(self, name, session, input_shape, input_node=None,
+            off_probability=0.5, save_location=None):
+        """
+        String -> tf.Session -> [Int] -> tf.Tensor? -> Float? -> String?
+            -> StochasticBinaryUnitLayer
+        """
+        self.off_probability = off_probability
+        super().__init__(name, session, input_shape, input_node, save_location)
+
+    def noise_function(self, input_node):
+        """
+        tf.Tensor -> (tf.Tensor, [tf.Variable])
+        """
+        uniform = tf.random_uniform(self.input_shape, 0, 1,
+            name=self.extend_name('random_uniform'))
+        is_off = tf.less(self.off_probability, uniform,
+            name=self.extend_name('is_off'))
+        return tf.multiply(input_node, tf.to_float(is_off),
+            name=self.extend_name('output_node')), []
