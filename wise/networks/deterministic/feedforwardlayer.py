249c6725f0c1cf6a4de50de04f0572fd6821b465
aatack <alexanderjtatack@gmail.com>
2018-10-19 02:42:17 +0100

    Edit feedforward layer to allow batch normalisation and verify

--- wise/networks/deterministic/feedforwardlayer.py
+++ wise/networks/deterministic/feedforwardlayer.py
@@ -1,5 +1,6 @@
 from wise.networks.layer import Layer
 from wise.networks.activation import Activation
+from wise.networks.stochastic.noise import StochasticBinaryUnitLayer
 from wise.util.tensors import glorot_initialised_vars
 import tensorflow as tf
 
@@ -10,7 +11,8 @@ class FeedforwardLayer(Layer):
     """
 
     def __init__(self, name, session, input_shape, output_shape,
-            activation=Activation.DEFAULT, input_node=None, save_location=None):
+            activation=Activation.DEFAULT, input_node=None,
+            batch_normalisation=False, save_location=None):
         """
         String -> tf.Session -> [Int] -> [Int] -> (tf.Tensor -> String -> tf.Tensor)
             -> tf.Tensor? -> String? -> Network
@@ -25,6 +27,10 @@ class FeedforwardLayer(Layer):
         self.biases = None
         self.after_biases = None
 
+        self.batch_normalisation = batch_normalisation
+        self.batch_normalised_weights = None
+        self.batch_normalised_biases = None
+
         self._initialise()
 
     def _initialise(self):
@@ -32,12 +38,23 @@ class FeedforwardLayer(Layer):
         () -> ()
         Initialise the tensorflow tensors that make up the layer.
         """
+        weights_shape = reversed(self.input_shape) + self.output_shape
         self.weights = glorot_initialised_vars(self.extend_name('weights'),
-            reversed(self.input_shape) + self.output_shape)
-        self.after_weights = tf.tensordot(self.input_node, self.weights,
+            weights_shape)
+        self.batch_normalised_weights = StochasticBinaryUnitLayer(
+            self.extend_name('batch_normalised_weights'), self.get_session(),
+            weights_shape, input_node=self.weights).get_output_node() \
+            if self.batch_normalisation else self.weights
+
+        self.after_weights = tf.tensordot(self.input_node, self.batch_normalised_weights,
             len(self.input_shape), name=self.extend_name('after_weights'))
         self.biases = glorot_initialised_vars(self.extend_name('biases'), self.output_shape)
-        self.after_biases = tf.add(self.after_weights, self.biases,
+        self.batch_normalised_biases = StochasticBinaryUnitLayer(
+            self.extend_name('batch_normalised_biases'), self.get_session(),
+            self.output_shape, input_node=self.biases).get_output_node() \
+            if self.batch_normalisation else self.biases
+
+        self.after_biases = tf.add(self.after_weights, self.batch_normalised_biases,
             name=self.extend_name('after_biases'))
         self.output_node = self.activation(self.after_biases,
             self.extend_name('output_node'))
