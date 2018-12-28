14d2532aa80646287fddfaac62cb6119885767d6
aatack <alexanderjtatack@gmail.com>
2018-12-28 01:09:49 +0000

    Implement function for creating a dataset of subimages

--- wise/preprocess/images.py
+++ wise/preprocess/images.py
@@ -1,6 +1,8 @@
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from skimage import color
+from math import ceil
+from random import randint
 from scipy.misc import imresize
 
 
@@ -54,3 +56,61 @@ def resize_image(w, h, image_tensor):
     new_size = (h, w, 3) if is_colour(
         image_tensor) else (h, w)
     return imresize(image_tensor, new_size)
+
+
+def dataset_from_image(image_tensor, n_samples, output_size,
+        output_io, sub_image_size, name):
+    """
+    numpy.ndarray -> Int -> (Int, Int) -> IO
+        -> ((Int, Int) -> ((Int, Int), (Int, Int)))
+        -> String -> ()
+    Create a dataset of images by taking random sections
+    of a larger image, resizing them to the desired output
+    size, and saving them using the provided IO object.
+    The sub image size is defined in terms of the coordinates
+    of the top left, and its height and width, which in turn
+    are a function of the image size.
+    """
+    file_name = '{}-{}'.format(name, '{}')
+    image_shape = image_tensor.shape[:2]
+    for i in range(n_samples):
+        ((top, left), (height, width)) = \
+            sub_image_size(image_shape)
+        if len(image_tensor.shape) == 3:
+            sub = image_tensor[
+                top:top + height, left:left + width, :]
+        else:
+            sub = image_tensor[
+                top:top + height, left:left + width]
+        sub = resize_image(output_size[0], output_size[1], sub)
+        output_io.save_image(sub, file_name.format(i))
+
+
+def squares_from_image(image_tensor, n_samples, output_size,
+        sub_image_size_range, output_io, name):
+    """
+    numpy.ndarray -> Int -> (Int, Int) -> (Float, Float)
+        -> IO -> String -> ()
+    Create a dataset of squares from a larger image by
+    taking random patches of it.  The size and position
+    of each image is determined as a function of the image
+    shape, where the maximum and minimum side lengths are
+    given as a proportion of the shortest edge of the large
+    image.
+    """
+    size_min, size_max = sub_image_size_range
+    def sub_image_size(image_shape):
+        h, w = image_shape
+        if h < w:
+            side_length = randint(
+                ceil(h * size_min), ceil(h * size_max))
+        else:
+            side_length = randint(
+                ceil(w * size_min), ceil(w * size_max))
+
+        top = randint(0, h - side_length - 1)
+        left = randint(0, w - side_length - 1)
+        return (top, left), (side_length, side_length)
+
+    dataset_from_image(image_tensor, n_samples, output_size,
+        output_io, sub_image_size, name)
