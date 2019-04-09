c4892eb8d4bd57d696d7f343d5b82286ea63a2b5
aatack <alexanderjtatack@gmail.com>
2019-04-09 14:03:12 +0100

    Add data logging to training

--- wise/training/routines.py
+++ wise/training/routines.py
@@ -13,11 +13,13 @@ def fit(
 ):
     """
     tf.Session -> tf.Operation -> FeedDictSampler -> Int -> Int -> Int
-        -> [(String, tf.Node)]? -> Int? -> ()
+        -> [(String, tf.Node)]? -> Int? -> [Dict]
     Fit the network to the data sampled from the given sampler.  If
     any nodes are given in the metrics field, they will be printed after
-    each epoch along with the epoch number.
+    each epoch along with the epoch number.  Returns a list of dictionaries
+    containing the value of each metric at each step.
     """
+    data = []
     log = metrics is not None
     try:
         for epoch in range(epochs):
@@ -28,17 +30,20 @@ def fit(
                 ceil(steps_per_epoch / batch_size),
                 batch_size,
             )
+            metric_results = session.run(
+                [node for _, node in metrics],
+                feed_dict=sampler.batch(evaluation_sample_size)
+                if sampler is not None
+                else None,
+            )
+
+            data.append({"Epoch": epoch + 1})
+            name_results = [("Epoch", epoch + 1)]
+            for name, result in (name, _), result in zip(metrics, metric_results):
+                data[-1][name] = result
+                name_results.append(name, result)
+
             if log:
-                metric_results = session.run(
-                    [node for _, node in metrics],
-                    feed_dict=sampler.batch(evaluation_sample_size)
-                    if sampler is not None
-                    else None,
-                )
-                name_results = [
-                    (name, result) for (name, _), result in zip(metrics, metric_results)
-                ]
-                name_results.insert(0, ("Epoch", epoch + 1))
                 print(
                     "\t".join(
                         [
@@ -58,6 +63,7 @@ def fit(
                     epoch + 1, epochs, str(100 * ((epoch + 1) / epochs))[:4]
                 )
             )
+    return data
 
 
 def perform_epoch(session, optimise_op, sampler, batches, batch_size):
